ff0660f95ef2ce730eff9d8dc2affdfb87cf24f9
nikkek <112384232+nikkek@users.noreply.github.com>
2026-07-25 21:47:36 +0300

    3.07. Assignment 1

--- todo/tremolo_plugin/include/Tremolo/Tremolo.h
+++ todo/tremolo_plugin/include/Tremolo/Tremolo.h
@@ -20,7 +20,9 @@ public:
       // generate the LFO value
       const auto lfoValue = lfo.processSample(0.f);
 
-      // TODO: calculate the modulation value
+      // calculate the modulation value
+      constexpr auto modulationDepth = 0.4f;
+      const auto modulationValue = modulationDepth * lfoValue + 1.f;
 
       // for each channel sample in the frame
       for (const auto channelIndex :
@@ -28,8 +30,8 @@ public:
         // get the input sample
         const auto inputSample = buffer.getSample(channelIndex, frameIndex);
 
-        // TODO: modulate the sample
-        const auto outputSample = 0.1f * lfoValue;  // inputSample;
+        // modulate the sample
+        const auto outputSample = inputSample * modulationValue;
 
         // set the output sample
         buffer.setSample(channelIndex, frameIndex, outputSample);
