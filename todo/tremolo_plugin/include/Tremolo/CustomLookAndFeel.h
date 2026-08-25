ed20ad54ad1b72753d8db4b8bbe6a4bcfd8654f7
nikkek <112384232+nikkek@users.noreply.github.com>
2026-08-25 14:25:26 +0300

    5.15. Assignment 1

--- todo/tremolo_plugin/include/Tremolo/CustomLookAndFeel.h
+++ todo/tremolo_plugin/include/Tremolo/CustomLookAndFeel.h
@@ -29,6 +29,33 @@ public:
                         const float rotaryEndAngle,
                         juce::Slider& slider) override;
 
+  void drawComboBox(juce::Graphics& g,
+                    int width,
+                    int height,
+                    bool,
+                    int,
+                    int,
+                    int,
+                    int,
+                    juce::ComboBox& box) override;
+
+  juce::Font getComboBoxFont(juce::ComboBox&) override {
+    return geistBold().withPointHeight(12.f);
+  }
+
+  juce::Font getPopupMenuFont() override {
+    return geistRegular().withPointHeight(12.f);
+  }
+
+  juce::Path getTickShape(float height) override { return {}; }
+
+  void positionComboBoxText(juce::ComboBox&,
+                            juce::Label& labelToPosition) override;
+
+  juce::PopupMenu::Options getOptionsForComboBoxPopupMenu(
+      juce::ComboBox&,
+      juce::Label&) override;
+
   static juce::FontOptions getSideLabelFonts() {
     return geistRegular().withPointHeight(10.f);
   }
--- todo/tremolo_plugin/source/CustomLookAndFeel.cpp
+++ todo/tremolo_plugin/source/CustomLookAndFeel.cpp
@@ -1,4 +1,24 @@
 namespace tremolo {
+
+namespace {
+void drawButtonInset(juce::Rectangle<float> bounds, juce::Graphics& g) {
+  auto outlineGradient = juce::ColourGradient::vertical(
+      juce::Colour{0xFF008171}, juce::Colour{0xFF004d44}, bounds);
+  g.setGradientFill(outlineGradient);
+  g.fillRoundedRectangle(bounds.toFloat(), 6.f);
+}
+
+void drawGreenGradientButton(const juce::Rectangle<float> bounds,
+                             juce::Graphics& g) {
+  auto buttonGradient = juce::ColourGradient::vertical(
+      juce::Colour{0xFF14ab7b}, juce::Colour{0xFF189076}, bounds);
+
+  buttonGradient.addColour(0.73, juce::Colour{0xFF009b77});
+  g.setGradientFill(buttonGradient);
+  g.fillRoundedRectangle(bounds.toFloat(), 4.f);
+}
+}  // namespace
+
 CustomLookAndFeel::CustomLookAndFeel() {
   setColour(juce::PopupMenu::backgroundColourId, juce::Colour{0xff14ab7b});
   setColour(juce::PopupMenu::textColourId, juce::Colours::beige);
@@ -21,13 +41,11 @@ void CustomLookAndFeel::drawToggleButton(juce::Graphics& g,
                                          juce::ToggleButton& button,
                                          bool shouldDrawButtonAsHighlighted,
                                          bool shouldDrawButtonAsDown) {
-  const auto bounds = button.getLocalBounds().reduced(2);
+  const auto bounds = button.getLocalBounds().toFloat().reduced(2);
 
-  auto outlineBounds = button.getLocalBounds();
-  auto outlineGradient = juce::ColourGradient::vertical(
-      juce::Colour{0xFF008171}, juce::Colour{0xFF004d44}, outlineBounds);
-  g.setGradientFill(outlineGradient);
-  g.fillRoundedRectangle(outlineBounds.toFloat(), 6.f);
+  const auto outlineBounds = button.getLocalBounds().toFloat();
+
+  drawButtonInset(outlineBounds, g);
 
   if (button.getToggleState()) {
     auto buttonGradient = juce::ColourGradient::vertical(
@@ -124,7 +142,7 @@ void CustomLookAndFeel::drawRotarySlider(juce::Graphics& g,
   g.setGradientFill(gloss);
   g.fillEllipse(glossBounds);
 
-  // Gloss
+  // Shadow
   juce::ColourGradient shadow(
       juce::Colours::black.withAlpha(0.0f), boundsInner.getX(),
       boundsInner.getCentreY() + 10, juce::Colours::black.withAlpha(0.1f),
@@ -132,6 +150,52 @@ void CustomLookAndFeel::drawRotarySlider(juce::Graphics& g,
 
   g.setGradientFill(shadow);
   g.fillEllipse(boundsInner);
+}
+
+void CustomLookAndFeel::drawComboBox(juce::Graphics& g,
+                                     int width,
+                                     int height,
+                                     bool,
+                                     int,
+                                     int,
+                                     int,
+                                     int,
+                                     juce::ComboBox& box) {
+  const auto bounds = box.getLocalBounds().toFloat();
+  drawButtonInset(bounds, g);
+
+  const auto buttonBounds = bounds.reduced(2);
+  drawGreenGradientButton(buttonBounds, g);
+
+  auto arrowBounds = buttonBounds.reduced(8, 9);
+  arrowBounds.removeFromLeft(104);
+
+  juce::Path arrow;
+  arrow.startNewSubPath(arrowBounds.getTopLeft());
+  arrow.lineTo(arrowBounds.getTopRight());
+  arrow.lineTo(arrowBounds.getCentreX(), arrowBounds.getBottom());
+  arrow.closeSubPath();
+
+  g.setColour(juce::Colours::beige);
+  g.fillPath(arrow);
+}
+
+void CustomLookAndFeel::positionComboBoxText(juce::ComboBox& box,
+                                             juce::Label& labelToPosition) {
+  auto bounds = box.getLocalBounds().reduced(10, 6);
+  bounds.removeFromRight(12);
+  labelToPosition.setBounds(bounds);
+  labelToPosition.setJustificationType(juce::Justification::centred);
+  labelToPosition.setFont(getComboBoxFont(box));
+}
+
+juce::PopupMenu::Options CustomLookAndFeel::getOptionsForComboBoxPopupMenu(
+    juce::ComboBox& box,
+    juce::Label& label) {
+  return LookAndFeel_V4::getOptionsForComboBoxPopupMenu(box, label)
+      .withStandardItemHeight(24)
+      .withMinimumWidth(128)
+      .withItemThatMustBeVisible(0);
 }
 
 juce::FontOptions CustomLookAndFeel::geistRegular() {
